756143b23aa2f793bd51cc6d1bbac8d447b18261
rzavodsky <zavodsky.richard1@gmail.com>
2022-12-02 22:25:32 +0100

    Added Login form

---
+++ UniversitySystem/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var people = DBConnection.DB.People;
+            var query = from p in people
+                        where p.loginUsername == usernameInput.Text
+                        && p.loginPassword == passwordInput.Text
+                        select p;
+            if (query.Count() == 1) 
+            {
+                var f2 = new StudentListForm();
+                this.Hide();
+                f2.Show();
+            }
+        }
+    }
+}
