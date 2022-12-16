7c760dfc71383320657125ac92e168eecb4e0f8d
Kappaonly <matuskokrivda@gmail.com>
2022-12-16 23:29:43 +0100

    Added Teacher Update Form

---
+++ UniversitySystem/TeacherUpdateForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace UniversitySystem
+{
+    partial class TeacherUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.facultyInput = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.genderInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cityInput = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.countryInput = new System.Windows.Forms.ComboBox();
+            this.firstNameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.birthDateInput = new System.Windows.Forms.DateTimePicker();
+            this.lastNameInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(55, 112);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Faculty:";
+            // 
+            // facultyInput
+            // 
+            this.facultyInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.facultyInput.FormattingEnabled = true;
+            this.facultyInput.Location = new System.Drawing.Point(127, 109);
+            this.facultyInput.Margin = new System.Windows.Forms.Padding(4);
+            this.facultyInput.Name = "facultyInput";
+            this.facultyInput.Size = new System.Drawing.Size(193, 24);
+            this.facultyInput.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(373, 16);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Gender:";
+            // 
+            // genderInput
+            // 
+            this.genderInput.Location = new System.Drawing.Point(443, 13);
+            this.genderInput.Margin = new System.Windows.Forms.Padding(4);
+            this.genderInput.Name = "genderInput";
+            this.genderInput.Size = new System.Drawing.Size(185, 22);
+            this.genderInput.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 80);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 16);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "City:";
+            // 
+            // cityInput
+            // 
+            this.cityInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityInput.FormattingEnabled = true;
+            this.cityInput.Location = new System.Drawing.Point(443, 77);
+            this.cityInput.Margin = new System.Windows.Forms.Padding(4);
+            this.cityInput.Name = "cityInput";
+            this.cityInput.Size = new System.Drawing.Size(185, 24);
+            this.cityInput.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(373, 47);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Country:";
+            // 
+            // countryInput
+            // 
+            this.countryInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.countryInput.FormattingEnabled = true;
+            this.countryInput.Location = new System.Drawing.Point(443, 44);
+            this.countryInput.Margin = new System.Windows.Forms.Padding(4);
+            this.countryInput.Name = "countryInput";
+            this.countryInput.Size = new System.Drawing.Size(185, 24);
+            this.countryInput.TabIndex = 24;
+            // 
+            // firstNameInput
+            // 
+            this.firstNameInput.Location = new System.Drawing.Point(127, 13);
+            this.firstNameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.firstNameInput.Name = "firstNameInput";
+            this.firstNameInput.Size = new System.Drawing.Size(193, 22);
+            this.firstNameInput.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Birth Date:";
+            // 
+            // birthDateInput
+            // 
+            this.birthDateInput.Location = new System.Drawing.Point(127, 77);
+            this.birthDateInput.Margin = new System.Windows.Forms.Padding(4);
+            this.birthDateInput.Name = "birthDateInput";
+            this.birthDateInput.Size = new System.Drawing.Size(193, 22);
+            this.birthDateInput.TabIndex = 22;
+            // 
+            // lastNameInput
+            // 
+            this.lastNameInput.Location = new System.Drawing.Point(127, 45);
+            this.lastNameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.lastNameInput.Name = "lastNameInput";
+            this.lastNameInput.Size = new System.Drawing.Size(193, 22);
+            this.lastNameInput.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Last Name:";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(187, 160);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 17;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(351, 160);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.TabIndex = 16;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // TeacherUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 254);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.facultyInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.genderInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cityInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.countryInput);
+            this.Controls.Add(this.firstNameInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.birthDateInput);
+            this.Controls.Add(this.lastNameInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.confirmButton);
+            this.Name = "TeacherUpdateForm";
+            this.Text = "TeacherUpdateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox facultyInput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox genderInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cityInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox countryInput;
+        private System.Windows.Forms.TextBox firstNameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker birthDateInput;
+        private System.Windows.Forms.TextBox lastNameInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button confirmButton;
+    }
+}
---
+++ UniversitySystem/TeacherUpdateForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class TeacherUpdateForm : Form
+    {
+        private Person teacher;
+        public TeacherUpdateForm(Person teacher)
+        {
+            InitializeComponent();
+            this.teacher = teacher;
+            InitializeComponent();
+            InitializeCountryDropDown();
+            InitializeCityDropDown();
+            InitializeFacultyDropDown();
+            InitializeFields();
+        }
+
+        private void InitializeFacultyDropDown()
+        {
+            facultyInput.DisplayMember = "name";
+            facultyInput.DataSource = DBConnection.DB.Faculties.ToList();
+        }
+
+        private void InitializeFields()
+        {
+            if (this.teacher == null) return;
+
+            firstNameInput.Text = this.teacher.firstName;
+            lastNameInput.Text = this.teacher.lastName;
+            birthDateInput.Value = this.teacher.birthDate;
+            genderInput.Text = this.teacher.gender;
+            countryInput.SelectedItem = this.teacher.City1.Country1;
+            cityInput.SelectedItem = this.teacher.City1;
+            facultyInput.SelectedItem = this.teacher.Faculty;
+        }
+        private void InitializeCityDropDown()
+        {
+            cityInput.DisplayMember = "name";
+            cityInput.DataSource = ((Country)this.countryInput.SelectedItem).Cities.ToList();
+        }
+
+        private void InitializeCountryDropDown()
+        {
+            countryInput.DisplayMember = "name";
+            countryInput.DataSource = DBConnection.DB.Countries.ToList();
+        }
+
+        private void countryInput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitializeCityDropDown();
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (this.teacher == null)
+            {
+                this.teacher = new Person();
+                DBConnection.DB.People.Add(this.teacher);
+            }
+
+            this.teacher.firstName = firstNameInput.Text;
+            this.teacher.lastName = lastNameInput.Text;
+            this.teacher.birthDate = birthDateInput.Value;
+            this.teacher.gender = genderInput.Text;
+            this.teacher.City1 = (City)cityInput.SelectedItem;
+           
+            if (this.teacher.loginUsername == null)
+            {
+                var sameNamedPeople = from p in DBConnection.DB.People
+                                      where p.lastName.ToLower() == this.teacher.lastName.ToLower()
+                                      select p;
+                this.teacher.loginUsername = this.teacher.lastName.ToLower() + (sameNamedPeople.Count() + 1);
+            }
+
+            if (this.teacher.loginPassword == null)
+            {
+                this.teacher.loginPassword = "password";
+
+            }
+
+            this.teacher.Faculty = (Faculty)facultyInput.SelectedItem;
+            this.teacher.isTeacher = true;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
