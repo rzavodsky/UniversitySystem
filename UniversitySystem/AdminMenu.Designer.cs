c6534003d2660d4329f15b1799f6d32f9d430d40
rzavodsky <zavodsky.richard1@gmail.com>
2022-12-16 23:26:35 +0100

    Added Admin menu

---
+++ UniversitySystem/AdminMenu.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace UniversitySystem
+{
+    partial class AdminMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resourceSelector = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchInput = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(870, 343);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resource:";
+            // 
+            // resourceSelector
+            // 
+            this.resourceSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.resourceSelector.FormattingEnabled = true;
+            this.resourceSelector.Location = new System.Drawing.Point(71, 12);
+            this.resourceSelector.Name = "resourceSelector";
+            this.resourceSelector.Size = new System.Drawing.Size(121, 21);
+            this.resourceSelector.TabIndex = 2;
+            this.resourceSelector.SelectedIndexChanged += new System.EventHandler(this.resourceSelector_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search:";
+            // 
+            // searchInput
+            // 
+            this.searchInput.Location = new System.Drawing.Point(261, 12);
+            this.searchInput.Name = "searchInput";
+            this.searchInput.Size = new System.Drawing.Size(132, 20);
+            this.searchInput.TabIndex = 4;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(576, 12);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(98, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add ";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(680, 12);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(98, 23);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(784, 12);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(98, 23);
+            this.removeButton.TabIndex = 7;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // AdminMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 396);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.searchInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resourceSelector);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "AdminMenu";
+            this.Text = "University Management System - Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox resourceSelector;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchInput;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
---
+++ UniversitySystem/AdminMenu.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class AdminMenu : Form
+    {
+        private static Resource[] RESOURCES =
+        {
+            new Resource("Students", new string[]{"First Name", "Last Name", "Student Group", "Birth Date", "Credits"}, (dataGridView) =>
+            {
+                var students = from student in DBConnection.DB.People
+                           where student.isTeacher == false
+                           select student;
+                foreach (var student in students)
+                {
+                    dataGridView.Rows.Add(new object[] {
+                        student.id,
+                        student.firstName,
+                        student.lastName,
+                        student.StudentGroup1.name,
+                        student.birthDate.ToShortDateString(),
+                        student.studentCredits,
+                    });
+                }
+            }, (id) => new StudentUpdateForm(id), 
+               (id) => DBConnection.DB.People.Remove(DBConnection.DB.People.Find(id))),
+
+
+            new Resource("Teachers", new string[]{"First Name", "Last Name", "Faculty", "Birth Date"}, (dataGridView) =>
+            {
+                var teachers = from teacher in DBConnection.DB.People
+                               where teacher.isTeacher == true
+                               select teacher;
+                foreach (var teacher in teachers)
+                {
+                    dataGridView.Rows.Add(new object[] {
+                        teacher.id,
+                        teacher.firstName,
+                        teacher.lastName,
+                        teacher.Faculty.shortcut,
+                        teacher.birthDate.ToShortDateString(),
+                    });
+                }
+            }, (id) => null, (id) => { }),
+        };
+
+        public AdminMenu()
+        {
+            InitializeComponent();
+            resourceSelector.DataSource = RESOURCES;
+            resourceSelector.DisplayMember = "Name";
+            InitializeDataGridView();
+            PopulateDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            var resource = (Resource)resourceSelector.SelectedItem;
+            dataGridView.Columns.Clear();
+            dataGridView.Columns.Add("Id", "Id");
+            dataGridView.Columns[0].Visible = false;
+            foreach (var column in resource.Columns)
+            {
+                dataGridView.Columns.Add(column, column);
+            }
+
+        }
+
+        private void PopulateDataGridView()
+        {
+            var resource = (Resource)resourceSelector.SelectedItem;
+            dataGridView.Rows.Clear();
+            resource.PopulationFunction(dataGridView);
+        }
+
+        private void resourceSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitializeDataGridView();
+            PopulateDataGridView();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var resource = (Resource)resourceSelector.SelectedItem;
+            var form = resource.OpenForm(null);
+            form.Show();
+            form.FormClosed += (object _sender, FormClosedEventArgs _e) => PopulateDataGridView();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            var resource = (Resource)resourceSelector.SelectedItem;
+            var form = resource.OpenForm((int)dataGridView.SelectedRows[0].Cells[0].Value);
+            form.Show();
+            form.FormClosed += (object _sender, FormClosedEventArgs _e) => PopulateDataGridView();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            var resource = (Resource)resourceSelector.SelectedItem;
+            resource.DeleteFunction((int)dataGridView.SelectedRows[0].Cells[0].Value);
+            DBConnection.DB.SaveChanges();
+            PopulateDataGridView();
+        }
+    }
+
+    internal class Resource
+    {
+        public Resource(string name, string[] columns, Action<DataGridView> populationFunction, Func<int?, Form> openForm, Action<int> deleteFunction)
+        {
+            Name = name;
+            Columns = columns;
+            PopulationFunction = populationFunction;
+            OpenForm = openForm;
+            DeleteFunction = deleteFunction;
+        }
+
+        public string Name { get; set; }
+        public string[] Columns { get; set; }
+        public Action<DataGridView> PopulationFunction { get; set; }
+        public Func<int?, Form> OpenForm { get; set; }
+        public Action<int> DeleteFunction { get; set; }
+    }
+  
+}
