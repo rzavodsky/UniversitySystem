07483fa5fc260affe2debb42349dd2d0c4abb251
Kappaonly <matuskokrivda@gmail.com>
2022-12-17 01:15:31 +0100

    Added CountryUpdateForm

---
+++ UniversitySystem/CountryUpdateForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace UniversitySystem
+{
+    partial class CountryUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameInput
+            // 
+            this.NameInput.Location = new System.Drawing.Point(73, 13);
+            this.NameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.NameInput.Name = "NameInput";
+            this.NameInput.Size = new System.Drawing.Size(193, 22);
+            this.NameInput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(70, 87);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(234, 87);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.TabIndex = 6;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // CountryUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 167);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.NameInput);
+            this.Controls.Add(this.label1);
+            this.Name = "CountryUpdateForm";
+            this.Text = "CountryUpdateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button confirmButton;
+    }
+}
---
+++ UniversitySystem/CountryUpdateForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class CountryUpdateForm : Form
+    {
+        private Country country;
+        public CountryUpdateForm(int? countryId)
+        {
+            if (countryId == null)
+            {
+                this.country = null;
+            }
+            else
+            {
+                this.country = DBConnection.DB.Countries.Find(countryId);
+            }
+
+            InitializeComponent();
+            InitializeFields();
+        }
+
+        private void InitializeFields()
+        {
+            if (this.country == null) return;
+            NameInput.Text = this.country.name;
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (this.country == null)
+            {
+                this.country = new Country();
+                DBConnection.DB.Countries.Add(this.country);
+            }
+
+            this.country.name = NameInput.Text;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
