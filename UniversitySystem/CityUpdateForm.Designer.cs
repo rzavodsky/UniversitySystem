29e0d998ee0747307fd4be83b09da49ee4a416f7
Kappaonly <matuskokrivda@gmail.com>
2022-12-17 01:21:06 +0100

    Added city Update Form

---
+++ UniversitySystem/CityUpdateForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace UniversitySystem
+{
+    partial class CityUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.cityNameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.countryInput = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 16);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Country:";
+            // 
+            // cityNameInput
+            // 
+            this.cityNameInput.Location = new System.Drawing.Point(66, 15);
+            this.cityNameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.cityNameInput.Name = "cityNameInput";
+            this.cityNameInput.Size = new System.Drawing.Size(193, 22);
+            this.cityNameInput.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Name:";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(166, 87);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 19;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(330, 87);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.TabIndex = 18;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click_1);
+            // 
+            // countryInput
+            // 
+            this.countryInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.countryInput.FormattingEnabled = true;
+            this.countryInput.Location = new System.Drawing.Point(359, 13);
+            this.countryInput.Margin = new System.Windows.Forms.Padding(4);
+            this.countryInput.Name = "countryInput";
+            this.countryInput.Size = new System.Drawing.Size(185, 24);
+            this.countryInput.TabIndex = 20;
+            // 
+            // CityUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 180);
+            this.Controls.Add(this.countryInput);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cityNameInput);
+            this.Controls.Add(this.label1);
+            this.Name = "CityUpdateForm";
+            this.Text = "CityUpdateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox cityNameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.ComboBox countryInput;
+    }
+}
---
+++ UniversitySystem/CityUpdateForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class CityUpdateForm : Form
+    
+    {
+     private City city;
+        public CityUpdateForm(int? cityId)
+        {
+            if (cityId == null)
+            {
+                this.city = null;
+            }
+            else
+            {
+                this.city = DBConnection.DB.Cities.Find(cityId);
+            }
+            InitializeComponent();
+            InitializeCountryDropDown();
+            InitializeFields();
+        }
+        private void InitializeFields()
+        {
+            if (this.city == null) return;
+            cityNameInput.Text = this.city.name;
+            countryInput.SelectedItem = this.city.Country1;
+        }
+       
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+         private void InitializeCountryDropDown()
+        {
+            countryInput.DisplayMember = "name";
+            countryInput.DataSource = DBConnection.DB.Countries.ToList();
+        }
+
+        private void confirmButton_Click_1(object sender, EventArgs e)
+        {
+            if (this.city == null)
+            {
+                this.city = new City();
+                DBConnection.DB.Cities.Add(this.city);
+            }
+
+            this.city.name = cityNameInput.Text;
+            this.city.Country1 = (Country)countryInput.SelectedItem;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
+        }
+    }
+}
