76a3e6d3699e900d78fc9e4ecd3622b89c7dfcd5
Kappaonly <matuskokrivda@gmail.com>
2023-01-09 19:48:53 +0100

    Added Degree Type Update form

---
+++ UniversitySystem/DegreeTypeUpdateForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace UniversitySystem
+{
+    partial class DegreeTypeUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.creditsInput = new System.Windows.Forms.TextBox();
+            this.durationUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.durationUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 87);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Credits required:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Duration (semester):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Name:";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(149, 22);
+            this.nameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(208, 22);
+            this.nameInput.TabIndex = 12;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(257, 140);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.TabIndex = 9;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(39, 140);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // creditsInput
+            // 
+            this.creditsInput.Location = new System.Drawing.Point(149, 84);
+            this.creditsInput.Margin = new System.Windows.Forms.Padding(4);
+            this.creditsInput.Name = "creditsInput";
+            this.creditsInput.Size = new System.Drawing.Size(208, 22);
+            this.creditsInput.TabIndex = 16;
+            // 
+            // durationUpDown
+            // 
+            this.durationUpDown.Location = new System.Drawing.Point(149, 55);
+            this.durationUpDown.Name = "durationUpDown";
+            this.durationUpDown.Size = new System.Drawing.Size(208, 22);
+            this.durationUpDown.TabIndex = 18;
+            // 
+            // DegreeTypeUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 202);
+            this.Controls.Add(this.durationUpDown);
+            this.Controls.Add(this.creditsInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.cancelButton);
+            this.Name = "DegreeTypeUpdateForm";
+            this.Text = "Update Degree Type";
+            ((System.ComponentModel.ISupportInitialize)(this.durationUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox creditsInput;
+        private System.Windows.Forms.NumericUpDown durationUpDown;
+    }
+}
