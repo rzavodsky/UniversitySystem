bb0a4c0e03849744b2948c0ca7a4bfe2ba982783
rzavodsky <zavodsky.richard1@gmail.com>
2023-01-09 23:57:13 +0100

    Connected StudentGroupUpdateForm to the database

--- UniversitySystem/StudentGroupUpdateForm.Designer.cs
+++ UniversitySystem/StudentGroupUpdateForm.Designer.cs
@@ -31,7 +31,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.nameInput = new System.Windows.Forms.TextBox();
-            this.facultyInput = new System.Windows.Forms.ComboBox();
+            this.programmeInput = new System.Windows.Forms.ComboBox();
             this.confirmButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -39,57 +39,59 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Location = new System.Drawing.Point(15, 58);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.Size = new System.Drawing.Size(95, 20);
             this.label2.TabIndex = 14;
             this.label2.Text = "Programme:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 16);
+            this.label1.Location = new System.Drawing.Point(53, 20);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 13;
             this.label1.Text = "Name:";
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(102, 13);
-            this.nameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.nameInput.Location = new System.Drawing.Point(115, 16);
+            this.nameInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.nameInput.Name = "nameInput";
-            this.nameInput.Size = new System.Drawing.Size(208, 22);
+            this.nameInput.Size = new System.Drawing.Size(234, 26);
             this.nameInput.TabIndex = 12;
             // 
-            // facultyInput
+            // programmeInput
             // 
-            this.facultyInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.facultyInput.FormattingEnabled = true;
-            this.facultyInput.Location = new System.Drawing.Point(102, 43);
-            this.facultyInput.Margin = new System.Windows.Forms.Padding(4);
-            this.facultyInput.Name = "facultyInput";
-            this.facultyInput.Size = new System.Drawing.Size(208, 24);
-            this.facultyInput.TabIndex = 10;
+            this.programmeInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.programmeInput.FormattingEnabled = true;
+            this.programmeInput.Location = new System.Drawing.Point(115, 54);
+            this.programmeInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.programmeInput.Name = "programmeInput";
+            this.programmeInput.Size = new System.Drawing.Size(234, 28);
+            this.programmeInput.TabIndex = 10;
             // 
             // confirmButton
             // 
-            this.confirmButton.Location = new System.Drawing.Point(210, 106);
-            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Location = new System.Drawing.Point(236, 132);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.Size = new System.Drawing.Size(112, 35);
             this.confirmButton.TabIndex = 9;
             this.confirmButton.Text = "Confirm";
             this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(22, 106);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(25, 132);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.Size = new System.Drawing.Size(112, 35);
             this.cancelButton.TabIndex = 8;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -97,16 +99,17 @@
             // StudentGroupUpdateForm
             // 
             this.AcceptButton = this.confirmButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(383, 161);
+            this.ClientSize = new System.Drawing.Size(431, 201);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nameInput);
-            this.Controls.Add(this.facultyInput);
+            this.Controls.Add(this.programmeInput);
             this.Controls.Add(this.confirmButton);
             this.Controls.Add(this.cancelButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "StudentGroupUpdateForm";
             this.Text = "Update Student Group";
             this.ResumeLayout(false);
@@ -118,7 +121,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox nameInput;
-        private System.Windows.Forms.ComboBox facultyInput;
+        private System.Windows.Forms.ComboBox programmeInput;
         private System.Windows.Forms.Button confirmButton;
         private System.Windows.Forms.Button cancelButton;
     }
--- UniversitySystem/StudentGroupUpdateForm.cs
+++ UniversitySystem/StudentGroupUpdateForm.cs
@@ -12,9 +12,46 @@ namespace UniversitySystem
 {
     public partial class StudentGroupUpdateForm : Form
     {
-        public StudentGroupUpdateForm()
+        private StudentGroup studentGroup;
+        public StudentGroupUpdateForm(int? studentGroupId)
         {
+            if (studentGroupId != null)
+            {
+                this.studentGroup = DBConnection.DB.StudentGroups.Find(studentGroupId);
+            }
+
             InitializeComponent();
+            InitializeProgrammeDropdown();
+            InitializeFields();
+        }
+
+        private void InitializeProgrammeDropdown()
+        {
+            programmeInput.DisplayMember = "name";
+            programmeInput.DataSource = DBConnection.DB.Programmes.ToList();
+        }
+
+        private void InitializeFields()
+        {
+            if (this.studentGroup == null) return;
+
+            nameInput.Text = this.studentGroup.name;
+            programmeInput.SelectedItem = this.studentGroup.Programme1;
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (this.studentGroup == null)
+            {
+                this.studentGroup = new StudentGroup();
+                DBConnection.DB.StudentGroups.Add(this.studentGroup);
+            }
+
+            this.studentGroup.name = nameInput.Text;
+            this.studentGroup.Programme1 = (Programme)programmeInput.SelectedItem;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
         }
     }
 }
