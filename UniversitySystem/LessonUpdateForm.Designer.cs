58cac0f580d62b04e4a359c128721a21a447d878
rzavodsky <zavodsky.richard1@gmail.com>
2023-01-11 17:29:36 +0100

    Connected LessonUpdateForm to the database

--- UniversitySystem/LessonUpdateForm.Designer.cs
+++ UniversitySystem/LessonUpdateForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UniversitySystem
 {
-    partial class LessonForm
+    partial class LessonUpdateForm
     {
         /// <summary>
         /// Required designer variable.
@@ -52,60 +52,84 @@
             // 
             // timeUpDown
             // 
-            this.timeUpDown.Location = new System.Drawing.Point(146, 84);
+            this.timeUpDown.Location = new System.Drawing.Point(110, 68);
+            this.timeUpDown.Margin = new System.Windows.Forms.Padding(2);
             this.timeUpDown.Maximum = new decimal(new int[] {
-            24,
+            19,
+            0,
+            0,
+            0});
+            this.timeUpDown.Minimum = new decimal(new int[] {
+            7,
             0,
             0,
             0});
             this.timeUpDown.Name = "timeUpDown";
-            this.timeUpDown.Size = new System.Drawing.Size(220, 22);
+            this.timeUpDown.Size = new System.Drawing.Size(165, 20);
             this.timeUpDown.TabIndex = 45;
+            this.timeUpDown.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.timeUpDown.ValueChanged += new System.EventHandler(this.timeUpDown_ValueChanged);
             // 
             // durationUpDown
             // 
-            this.durationUpDown.Location = new System.Drawing.Point(146, 112);
+            this.durationUpDown.Location = new System.Drawing.Point(110, 91);
+            this.durationUpDown.Margin = new System.Windows.Forms.Padding(2);
+            this.durationUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.durationUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.durationUpDown.Name = "durationUpDown";
-            this.durationUpDown.Size = new System.Drawing.Size(220, 22);
+            this.durationUpDown.Size = new System.Drawing.Size(165, 20);
             this.durationUpDown.TabIndex = 44;
+            this.durationUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(96, 176);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(72, 143);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(42, 16);
+            this.label7.Size = new System.Drawing.Size(34, 13);
             this.label7.TabIndex = 42;
             this.label7.Text = "Type:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(77, 144);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(58, 117);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.Size = new System.Drawing.Size(50, 13);
             this.label6.TabIndex = 41;
             this.label6.Text = "Teacher:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(79, 114);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(59, 93);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 40;
             this.label5.Text = "Duration:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(98, 86);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(74, 70);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.Size = new System.Drawing.Size(33, 13);
             this.label4.TabIndex = 39;
             this.label4.Text = "Time:";
             // 
@@ -113,130 +137,131 @@
             // 
             this.weekDayInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.weekDayInput.FormattingEnabled = true;
-            this.weekDayInput.Location = new System.Drawing.Point(146, 53);
-            this.weekDayInput.Margin = new System.Windows.Forms.Padding(4);
+            this.weekDayInput.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.weekDayInput.Location = new System.Drawing.Point(110, 43);
             this.weekDayInput.Name = "weekDayInput";
-            this.weekDayInput.Size = new System.Drawing.Size(220, 24);
+            this.weekDayInput.Size = new System.Drawing.Size(166, 21);
             this.weekDayInput.TabIndex = 37;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 208);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(47, 169);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 35;
             this.label3.Text = "Classroom:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 24);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(58, 20);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 34;
-            this.label2.Text = "Subject Name:";
+            this.label2.Text = "Subject:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 56);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(48, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 33;
             this.label1.Text = "Week Day:";
             // 
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(39, 296);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Location = new System.Drawing.Point(29, 240);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 32;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // confirmButton
             // 
-            this.confirmButton.Location = new System.Drawing.Point(266, 296);
-            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Location = new System.Drawing.Point(200, 240);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
             this.confirmButton.TabIndex = 31;
             this.confirmButton.Text = "Confirm";
             this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
             // 
             // subjectNameInput
             // 
             this.subjectNameInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.subjectNameInput.FormattingEnabled = true;
-            this.subjectNameInput.Location = new System.Drawing.Point(146, 21);
-            this.subjectNameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.subjectNameInput.Location = new System.Drawing.Point(110, 17);
             this.subjectNameInput.Name = "subjectNameInput";
-            this.subjectNameInput.Size = new System.Drawing.Size(220, 24);
+            this.subjectNameInput.Size = new System.Drawing.Size(166, 21);
             this.subjectNameInput.TabIndex = 47;
             // 
             // teacherInput
             // 
             this.teacherInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.teacherInput.FormattingEnabled = true;
-            this.teacherInput.Location = new System.Drawing.Point(146, 141);
-            this.teacherInput.Margin = new System.Windows.Forms.Padding(4);
+            this.teacherInput.Location = new System.Drawing.Point(110, 115);
             this.teacherInput.Name = "teacherInput";
-            this.teacherInput.Size = new System.Drawing.Size(220, 24);
+            this.teacherInput.Size = new System.Drawing.Size(166, 21);
             this.teacherInput.TabIndex = 48;
             // 
             // typeInput
             // 
             this.typeInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.typeInput.FormattingEnabled = true;
-            this.typeInput.Location = new System.Drawing.Point(146, 173);
-            this.typeInput.Margin = new System.Windows.Forms.Padding(4);
+            this.typeInput.Items.AddRange(new object[] {
+            "Lecture",
+            "Excercise"});
+            this.typeInput.Location = new System.Drawing.Point(110, 141);
             this.typeInput.Name = "typeInput";
-            this.typeInput.Size = new System.Drawing.Size(220, 24);
+            this.typeInput.Size = new System.Drawing.Size(166, 21);
             this.typeInput.TabIndex = 49;
             // 
             // classroomInput
             // 
             this.classroomInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.classroomInput.FormattingEnabled = true;
-            this.classroomInput.Location = new System.Drawing.Point(146, 205);
-            this.classroomInput.Margin = new System.Windows.Forms.Padding(4);
+            this.classroomInput.Location = new System.Drawing.Point(110, 167);
             this.classroomInput.Name = "classroomInput";
-            this.classroomInput.Size = new System.Drawing.Size(220, 24);
+            this.classroomInput.Size = new System.Drawing.Size(166, 21);
             this.classroomInput.TabIndex = 50;
             // 
             // studentGroupInput
             // 
             this.studentGroupInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.studentGroupInput.FormattingEnabled = true;
-            this.studentGroupInput.Location = new System.Drawing.Point(146, 237);
-            this.studentGroupInput.Margin = new System.Windows.Forms.Padding(4);
+            this.studentGroupInput.Location = new System.Drawing.Point(110, 193);
             this.studentGroupInput.Name = "studentGroupInput";
-            this.studentGroupInput.Size = new System.Drawing.Size(220, 24);
+            this.studentGroupInput.Size = new System.Drawing.Size(166, 21);
             this.studentGroupInput.TabIndex = 51;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(43, 240);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(32, 195);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(95, 16);
+            this.label8.Size = new System.Drawing.Size(79, 13);
             this.label8.TabIndex = 52;
             this.label8.Text = "Student Group:";
             // 
-            // LessonForm
+            // LessonUpdateForm
             // 
             this.AcceptButton = this.confirmButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(445, 383);
+            this.ClientSize = new System.Drawing.Size(334, 311);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.studentGroupInput);
             this.Controls.Add(this.classroomInput);
@@ -255,8 +280,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.confirmButton);
-            this.Name = "LessonForm";
-            this.Text = "LessonForm";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "LessonUpdateForm";
+            this.Text = "Update Lesson";
             ((System.ComponentModel.ISupportInitialize)(this.timeUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.durationUpDown)).EndInit();
             this.ResumeLayout(false);
---
+++ UniversitySystem/LessonUpdateForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class LessonUpdateForm : Form
+    {
+        private Lesson lesson;
+        public LessonUpdateForm(int? lessonId)
+        {
+            if (lessonId != null) 
+            { 
+                this.lesson = DBConnection.DB.Lessons.Find(lessonId);
+            }
+
+            InitializeComponent();
+            InitializeSubjectDropdown();
+            InitializeTeacherDropdown();
+            InitializeClassroomDropdown();
+            InitializeStudentGroupDropdown();
+            InitializeFields();
+        }
+
+        private void InitializeStudentGroupDropdown()
+        {
+            studentGroupInput.DataSource = DBConnection.DB.StudentGroups.ToList();
+            studentGroupInput.DisplayMember = "name";
+        }
+
+        private void InitializeClassroomDropdown()
+        {
+            classroomInput.DataSource = DBConnection.DB.Classrooms.ToList();
+            classroomInput.DisplayMember = "name";
+        }
+
+        private void InitializeTeacherDropdown()
+        {
+            teacherInput.DataSource = DBConnection.DB.People
+                .Where(p => p.isTeacher)
+                .Select(p => new TeacherRecord {
+                    Id = p.id,
+                    Name = p.firstName + " " + p.lastName,
+                })
+                .ToList();
+            teacherInput.DisplayMember = "Name";
+        }
+
+        private void InitializeSubjectDropdown()
+        {
+            subjectNameInput.DataSource = DBConnection.DB.Subjects.ToList();
+            subjectNameInput.DisplayMember = "name";
+        }
+
+        private void InitializeFields()
+        {
+            weekDayInput.SelectedIndex = 0;
+            typeInput.SelectedIndex = 0;
+            if (this.lesson == null) return;
+            subjectNameInput.SelectedItem = this.lesson.Subject1;
+            weekDayInput.SelectedIndex = this.lesson.weekDay;
+            timeUpDown.Value = this.lesson.time;
+            durationUpDown.Value = this.lesson.duration;
+            teacherInput.SelectedItem = new TeacherRecord
+            {
+                Id = this.lesson.Person.id,
+                Name = this.lesson.Person.firstName + " " + this.lesson.Person.lastName,
+            };
+            typeInput.SelectedItem = this.lesson.type;
+            classroomInput.SelectedItem = this.lesson.Classroom1;
+            studentGroupInput.SelectedItem = this.lesson.StudentGroup1;
+        }
+
+        private void timeUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            durationUpDown.Maximum = 20 - timeUpDown.Value;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (this.lesson == null)
+            {
+                this.lesson = new Lesson();
+                DBConnection.DB.Lessons.Add(this.lesson);
+            }
+
+            this.lesson.Subject1 = (Subject)subjectNameInput.SelectedItem;
+            this.lesson.weekDay = weekDayInput.SelectedIndex;
+            this.lesson.time = (int)timeUpDown.Value;
+            this.lesson.duration = (int)durationUpDown.Value;
+            this.lesson.Person = DBConnection.DB.People.Find(((TeacherRecord)teacherInput.SelectedItem).Id);
+            this.lesson.type = (string)typeInput.SelectedItem;
+            this.lesson.Classroom1 = (Classroom)classroomInput.SelectedItem;
+            this.lesson.StudentGroup1 = (StudentGroup)studentGroupInput.SelectedItem;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
+        }
+
+        private class TeacherRecord
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
