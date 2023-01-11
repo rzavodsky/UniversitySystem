e8cfb804b67a2dec1607d743cbb43e5d9e465baa
rzavodsky <zavodsky.richard1@gmail.com>
2023-01-11 17:30:36 +0100

    Connected SubjectUpdateForm to the database

--- UniversitySystem/SubjectUpdateForm.Designer.cs
+++ UniversitySystem/SubjectUpdateForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UniversitySystem
 {
-    partial class SubjectForm
+    partial class SubjectUpdateForm
     {
         /// <summary>
         /// Required designer variable.
@@ -55,59 +55,54 @@
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(105, 19);
-            this.nameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.nameInput.Location = new System.Drawing.Point(79, 15);
             this.nameInput.Name = "nameInput";
-            this.nameInput.Size = new System.Drawing.Size(220, 22);
+            this.nameInput.Size = new System.Drawing.Size(267, 20);
             this.nameInput.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 194);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(14, 187);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 12;
             this.label3.Text = "Description:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 22);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(38, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "Name:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 50);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(33, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 10;
             this.label1.Text = "Faculty:";
             // 
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(53, 266);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Location = new System.Drawing.Point(102, 294);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 9;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // confirmButton
             // 
-            this.confirmButton.Location = new System.Drawing.Point(225, 266);
-            this.confirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.confirmButton.Location = new System.Drawing.Point(231, 294);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(100, 28);
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
             this.confirmButton.TabIndex = 8;
             this.confirmButton.Text = "Confirm";
             this.confirmButton.UseVisualStyleBackColor = true;
@@ -117,113 +112,115 @@
             // 
             this.facultyInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.facultyInput.FormattingEnabled = true;
-            this.facultyInput.Location = new System.Drawing.Point(105, 47);
-            this.facultyInput.Margin = new System.Windows.Forms.Padding(4);
+            this.facultyInput.Location = new System.Drawing.Point(79, 67);
             this.facultyInput.Name = "facultyInput";
-            this.facultyInput.Size = new System.Drawing.Size(220, 24);
+            this.facultyInput.Size = new System.Drawing.Size(267, 21);
             this.facultyInput.TabIndex = 21;
             // 
             // descriptionTextBox
             // 
-            this.descriptionTextBox.Location = new System.Drawing.Point(105, 191);
-            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.descriptionTextBox.Location = new System.Drawing.Point(79, 184);
+            this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
-            this.descriptionTextBox.Size = new System.Drawing.Size(220, 22);
+            this.descriptionTextBox.Size = new System.Drawing.Size(267, 93);
             this.descriptionTextBox.TabIndex = 22;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 80);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(16, 94);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 23;
             this.label4.Text = "Excercises:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(37, 108);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(28, 117);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 24;
             this.label5.Text = "Lectures:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(45, 136);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(34, 139);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.Size = new System.Drawing.Size(42, 13);
             this.label6.TabIndex = 25;
             this.label6.Text = "Credits:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(29, 164);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(22, 162);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.Size = new System.Drawing.Size(54, 13);
             this.label7.TabIndex = 26;
             this.label7.Text = "Semester:";
             // 
             // semesterUpDown
             // 
-            this.semesterUpDown.Location = new System.Drawing.Point(105, 162);
+            this.semesterUpDown.Location = new System.Drawing.Point(79, 161);
+            this.semesterUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.semesterUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             this.semesterUpDown.Name = "semesterUpDown";
-            this.semesterUpDown.Size = new System.Drawing.Size(220, 22);
+            this.semesterUpDown.Size = new System.Drawing.Size(266, 20);
             this.semesterUpDown.TabIndex = 27;
             // 
             // creditsUpDown
             // 
-            this.creditsUpDown.Location = new System.Drawing.Point(105, 134);
+            this.creditsUpDown.Location = new System.Drawing.Point(79, 138);
+            this.creditsUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.creditsUpDown.Name = "creditsUpDown";
-            this.creditsUpDown.Size = new System.Drawing.Size(220, 22);
+            this.creditsUpDown.Size = new System.Drawing.Size(266, 20);
             this.creditsUpDown.TabIndex = 28;
             // 
             // lecturesUpDown
             // 
-            this.lecturesUpDown.Location = new System.Drawing.Point(105, 106);
+            this.lecturesUpDown.Location = new System.Drawing.Point(79, 115);
+            this.lecturesUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lecturesUpDown.Name = "lecturesUpDown";
-            this.lecturesUpDown.Size = new System.Drawing.Size(220, 22);
+            this.lecturesUpDown.Size = new System.Drawing.Size(266, 20);
             this.lecturesUpDown.TabIndex = 29;
             // 
             // exercisesUpDown
             // 
-            this.exercisesUpDown.Location = new System.Drawing.Point(105, 78);
+            this.exercisesUpDown.Location = new System.Drawing.Point(79, 92);
+            this.exercisesUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exercisesUpDown.Name = "exercisesUpDown";
-            this.exercisesUpDown.Size = new System.Drawing.Size(220, 22);
+            this.exercisesUpDown.Size = new System.Drawing.Size(266, 20);
             this.exercisesUpDown.TabIndex = 30;
             // 
             // shortcutInput
             // 
-            this.shortcutInput.Location = new System.Drawing.Point(105, 221);
-            this.shortcutInput.Margin = new System.Windows.Forms.Padding(4);
+            this.shortcutInput.Location = new System.Drawing.Point(79, 41);
             this.shortcutInput.Name = "shortcutInput";
-            this.shortcutInput.Size = new System.Drawing.Size(220, 22);
+            this.shortcutInput.Size = new System.Drawing.Size(267, 20);
             this.shortcutInput.TabIndex = 31;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(39, 224);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(29, 43);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(58, 16);
+            this.label8.Size = new System.Drawing.Size(50, 13);
             this.label8.TabIndex = 32;
             this.label8.Text = "Shortcut:";
             // 
-            // SubjectForm
+            // SubjectUpdateForm
             // 
             this.AcceptButton = this.confirmButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(445, 319);
+            this.ClientSize = new System.Drawing.Size(403, 340);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.shortcutInput);
             this.Controls.Add(this.exercisesUpDown);
@@ -242,7 +239,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.confirmButton);
-            this.Name = "SubjectForm";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "SubjectUpdateForm";
             this.Text = "SubjectForm";
             ((System.ComponentModel.ISupportInitialize)(this.semesterUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.creditsUpDown)).EndInit();
---
+++ UniversitySystem/SubjectUpdateForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystem
+{
+    public partial class SubjectUpdateForm : Form
+    {
+        private Subject subject;
+        public SubjectUpdateForm(int? subjectId)
+        {
+            if (subjectId != null)
+            {
+                this.subject = DBConnection.DB.Subjects.Find(subjectId);
+            }
+
+            InitializeComponent();
+            InitializeFacultyDropdown();
+            InitializeFields();
+        }
+
+        private void InitializeFields()
+        {
+            if (this.subject == null) return;
+            nameInput.Text = this.subject.name;
+            shortcutInput.Text = this.subject.shortcut;
+            facultyInput.SelectedItem = this.subject.Faculty1;
+            exercisesUpDown.Value = this.subject.excerciseAmount;
+            lecturesUpDown.Value = this.subject.lectureAmount;
+            creditsUpDown.Value = this.subject.credits;
+            semesterUpDown.Value = this.subject.semester;
+            descriptionTextBox.Text = this.subject.description;
+        }
+
+        private void InitializeFacultyDropdown()
+        {
+            facultyInput.DataSource = DBConnection.DB.Faculties.ToList();
+            facultyInput.DisplayMember = "name";
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (this.subject == null)
+            {
+                this.subject = new Subject();
+                DBConnection.DB.Subjects.Add(this.subject);
+            }
+
+            this.subject.name = nameInput.Text;
+            this.subject.shortcut = shortcutInput.Text;
+            this.subject.Faculty1 = (Faculty)facultyInput.SelectedItem;
+            this.subject.excerciseAmount = (int)exercisesUpDown.Value;
+            this.subject.lectureAmount = (int)lecturesUpDown.Value;
+            this.subject.credits = (int)creditsUpDown.Value;
+            this.subject.semester = (int)semesterUpDown.Value;
+            this.subject.description = descriptionTextBox.Text;
+
+            DBConnection.DB.SaveChanges();
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
