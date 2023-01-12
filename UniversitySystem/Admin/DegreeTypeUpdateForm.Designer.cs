a8bd8e361cf28fa1b17a015bce529824780e3eb8
rzavodsky <zavodsky.richard1@gmail.com>
2023-01-12 16:26:10 +0100

    Changed maximum of credits in degreetype

--- UniversitySystem/Admin/DegreeTypeUpdateForm.Designer.cs
+++ UniversitySystem/Admin/DegreeTypeUpdateForm.Designer.cs
@@ -43,47 +43,42 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 109);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(27, 71);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 15;
             this.label3.Text = "Credits required:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 69);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(9, 45);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Duration (semester):";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(106, 35);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(71, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 13;
             this.label1.Text = "Name:";
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(168, 28);
-            this.nameInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nameInput.Location = new System.Drawing.Point(112, 18);
             this.nameInput.Name = "nameInput";
-            this.nameInput.Size = new System.Drawing.Size(234, 26);
+            this.nameInput.Size = new System.Drawing.Size(157, 20);
             this.nameInput.TabIndex = 12;
             // 
             // confirmButton
             // 
-            this.confirmButton.Location = new System.Drawing.Point(289, 175);
-            this.confirmButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.confirmButton.Location = new System.Drawing.Point(193, 114);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(112, 35);
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
             this.confirmButton.TabIndex = 9;
             this.confirmButton.Text = "Confirm";
             this.confirmButton.UseVisualStyleBackColor = true;
@@ -92,10 +87,9 @@
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(44, 175);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cancelButton.Location = new System.Drawing.Point(29, 114);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(112, 35);
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 8;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -103,27 +97,32 @@
             // 
             // durationUpDown
             // 
-            this.durationUpDown.Location = new System.Drawing.Point(168, 69);
-            this.durationUpDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.durationUpDown.Location = new System.Drawing.Point(112, 45);
+            this.durationUpDown.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.durationUpDown.Name = "durationUpDown";
-            this.durationUpDown.Size = new System.Drawing.Size(234, 26);
+            this.durationUpDown.Size = new System.Drawing.Size(156, 20);
             this.durationUpDown.TabIndex = 18;
             // 
             // creditsUpDown
             // 
-            this.creditsUpDown.Location = new System.Drawing.Point(167, 107);
-            this.creditsUpDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.creditsUpDown.Location = new System.Drawing.Point(111, 70);
+            this.creditsUpDown.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.creditsUpDown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.creditsUpDown.Name = "creditsUpDown";
-            this.creditsUpDown.Size = new System.Drawing.Size(234, 26);
+            this.creditsUpDown.Size = new System.Drawing.Size(156, 20);
             this.creditsUpDown.TabIndex = 19;
             // 
             // DegreeTypeUpdateForm
             // 
             this.AcceptButton = this.confirmButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(465, 252);
+            this.ClientSize = new System.Drawing.Size(310, 164);
             this.Controls.Add(this.creditsUpDown);
             this.Controls.Add(this.durationUpDown);
             this.Controls.Add(this.label3);
@@ -132,7 +131,7 @@
             this.Controls.Add(this.nameInput);
             this.Controls.Add(this.confirmButton);
             this.Controls.Add(this.cancelButton);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "DegreeTypeUpdateForm";
             this.Text = "Update Degree Type";
             ((System.ComponentModel.ISupportInitialize)(this.durationUpDown)).EndInit();
